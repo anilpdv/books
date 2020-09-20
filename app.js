d10dccc6251de906a502ea48492b841d6ff037ae
anilpdv <pdvanil007@gmail.com>
2020-09-20 17:12:02 +0530

    fixing vulnerabilties and changing urls in libgen

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 // : importing the modules
-const express = require('express');
-const expressOasGenerator = require('express-oas-generator');
-const path = require('path');
-const cors = require('cors');
+const express = require("express");
+const expressOasGenerator = require("express-oas-generator");
+const path = require("path");
+const cors = require("cors");
 
 // : routes
-const searchRoute = require('./routes/libgen/search');
-const reviewRoute = require('./routes/good-reads/goodreads.js');
-const downloadRoute = require('./routes/libgen/download.js');
+const searchRoute = require("./routes/libgen/search");
+const reviewRoute = require("./routes/good-reads/goodreads.js");
+const downloadRoute = require("./routes/libgen/download.js");
 
 // : creating instance
 const app = express();
@@ -15,15 +15,15 @@ expressOasGenerator.init(app, {});
 app.use(cors());
 
 // : middleware
-app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use("/static", express.static(path.join(__dirname, "public")));
 
 // : creating routes
-app.use('/api/books', searchRoute);
-app.use('/api/book', reviewRoute);
-app.use('/api/book', downloadRoute);
+app.use("/api/books", searchRoute);
+app.use("/api/book", reviewRoute);
+app.use("/api/book", downloadRoute);
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log('server is started and listening to port 3000');
+  console.log("server is started and listening to port " + port);
   console.log(`listen to the server http://localhost:${port}`);
 });
--- routes/libgen/download.js
+++ routes/libgen/download.js
@@ -1,32 +1,32 @@
-const express = require('express');
-const axios = require('axios');
-const prettyError = require('pretty-error');
+const express = require("express");
+const axios = require("axios");
+const prettyError = require("pretty-error");
 
 const router = express.Router();
 const pe = new prettyError();
 
 // res.set('Content-Type', 'application/epub+zip');
-router.get('/download/:title', (req, res) => {
+router.get("/download/:title", (req, res) => {
   axios
-    .get('http://libgen.io/get.php?md5=' + req.query.md5, {
-      responseType: 'stream',
+    .get("http://libgen.is/get.php?md5=" + req.query.md5, {
+      responseType: "stream",
     })
-    .then(resp => {
+    .then((resp) => {
       resp.data.pipe(res);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(pe.render(err));
     });
 });
 
-router.get('/:id', (req, res) => {
-  const url = 'http://gen.lib.rus.ec/json.php?ids=' + req.params.id;
+router.get("/:id", (req, res) => {
+  const url = "http://gen.lib.rus.ec/json.php?ids=" + req.params.id;
   axios
     .get(url)
-    .then(result => {
+    .then((result) => {
       res.send(result.data);
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
     });
 });
